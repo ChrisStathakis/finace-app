f0bf809f964b8507aef370b8ad433a14e6d3ff9b
Zefarak <lirageika@hotmail.gr>
2021-12-31 16:02:59 +0200

    changed req

--- finace_app/settings.py
+++ finace_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PRODUCTION = True
+PRODUCTION = False
 
 ALLOWED_HOSTS = ['*', ] if PRODUCTION else ['*', ]
 
